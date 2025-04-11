c012286ff5adb699ff27693e0d4ecf06a85cbef5
jaypeelaurence <jaypeelaurencecocjin@gmail.com>
2025-04-12 00:17:55 +0800

    fix: added animations

--- src/ui/components/image.tsx
+++ src/ui/components/image.tsx
@@ -1,5 +1,6 @@
 import { cn } from '../../utils';
 import { FC } from 'react';
+import Button from './Button';
 
 interface PROPS {
   className?: string | string[];
@@ -8,11 +9,19 @@ interface PROPS {
 }
 
 const Image: FC<PROPS>= ({ className, src, alt }) => (
-  <img
-    alt={alt}
-    className={cn(className)}
-    src={src}
-  />
+  <Button onClick={() => {}} type='button' className={cn(className)}>
+    <img
+      alt={alt}
+      className={cn(
+        'transition-transform',
+        'duration-300',
+        'ease-in-out',
+        'hover:-translate-y-1',
+        className,
+      )}
+      src={src}
+    />
+  </Button>
 );
 
 export default Image;
